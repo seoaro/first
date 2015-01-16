f45fc6b41966dd3ae12dee556c6ee39b45df0e1f
ryungmin <ryungmin@gmail.com>
2015-01-16 11:24:22 +0900

    checkdate

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
-    function greeting() {
-        echo "Hello World!";
+    $result = checkdate(2, 29, 2006);
+    if ($result) {
+        echo "29 Feb 2006 is a valid date.";
+    }else{
+        echo "29 Feb 2006 is not a valid date.";
     }
-    greeting();
 ?>
