e0a0b97b8b1298b36ab557a24651dffc455d1999
ryungmin <ryungmin@gmail.com>
2015-01-16 11:46:09 +0900

    date()

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-    $result = checkdate(2, 29, 2006);
-    if ($result) {
-        echo "29 Feb 2006 is a valid date.";
-    }else{
-        echo "29 Feb 2006 is not a valid date.";
-    }
+    echo date("오늘은 Y년 m월 d일 입니다.") . "<BR>";
+    echo date("지금 시각은 H시 i분 s초 입니다.") . "<BR>";
+    echo date("오늘은 올해의 z번째 날이며 W번째 주입니다") . "<BR>";
+    echo date("이번달의 마지막 날은 t일 입니다.") . "<BR>";
 ?>
