1634f3792c20c4ebe8b934a7b824a93cd85b2150
ryungmin <ryungmin@gmail.com>
2015-01-19 12:44:45 +0900

    file

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <?php
-	echo date("오늘은 Y년 m월 d일 입니다.") . "<BR>";
-	echo date("지금 시각은 H시 i분 s초 입니다.") . "<BR>";
-	echo date("오늘은 올해의 z번째 날이며 W번째 주입니다") . "<BR>";
-	echo date("이번달의 마지막 날은 t일 입니다.") . "<BR>";
+//	$array = file('somefile.txt');
+//
+//	foreach ($array as $line_num => $line) {
+//		echo "#{$line_num} : $line <br>\n";
+//	}
+//
+//	$array = file('http://www.naver.com/');
+//
+//	foreach ($array as $line_num => $line) {
+//		echo htmlspecialchars($line) . " <br>\n";
+//	}
+	$filename = "filetest.txt";
 
-	$date = mktime(6, 28, 31, 6, 8, 2009);
-	echo date("주어진 날짜는 Y년 m월 d일 입니다.", $date). "<BR>";
-	echo date("주어진 시간은 H시 i분 s초 입니다.", $date). "<BR>";
+	$handle = fopen($filename, "rw");
+
+	if ($handle) {
+		echo "done <BR><BR>";
+	} else {
+		echo "false <BR>";
+	}
+	$string = "자극하는 PHP 프로그래밍";
+	fwrite($handle, $string);
+	//
+	$handle = fopen($filename, "r");
+	$contents = fread($handle, filesize($filename));
+	echo "$contents";
+	fclose($handle);
+
 ?>
