66895eb9f14db3c35e56533818b7810628c54264
ryungmin <ryungmin@gmail.com>
2015-01-19 15:58:34 +0900

    strstr(),  find some str 'ex) @ and print @xxxx.com'

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php
-	$haystack = "She is a very hot woman";
-	$needle = "hot";
-
-	$pos = strpos($haystack, $needle);
-
-	if ($pos === false)
-	{
-		echo "문자열을 찾지 못했습니다.";
-	}
-	else
-	{
-		echo "{$pos} 위치에서 문자열을 찾았습니다.";
-	}
+	$email = 'happybrown@naver.com';
+	$domain = strstr($email, '@');
+	echo $domain; //@naver.com이 출력됨
 ?>
