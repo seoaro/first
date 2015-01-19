8d9e8673a95739a4d2dfc9cb9c5adf1c3229d686
ryungmin <ryungmin@gmail.com>
2015-01-19 16:34:46 +0900

    text encode decode

--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
-	echo mt_rand();
-	echo "<br>";
-	echo mt_rand(1,10);
-	echo "<br>";
-	echo mt_rand(40000,50000);
-	echo "<br><br>";
-	echo mt_rand(1,45);
-	echo "<br>";
-	echo mt_rand(1,45);
-	echo "<br>";
-	echo mt_rand(1,45);
-	echo "<br>";
-	echo mt_rand(1,45);
-	echo "<br>";
-	echo mt_rand(1,45);
-	echo "<br>";
-	echo mt_rand(1,45);
+	$text = '행복한 브라운';
+	echo base64_encode($text) . "<br>";
+	$text = 'x+C6ucfRILrqtvO/7g==';
+	echo base64_decode($text) . "<br>" . "<br>";
+
+	$text = '가나12';
+	echo urlencode($text) . "<br>"; //%B0%A1%B3%AA12
+	$text = '%B0%A1%B3%AA12';
+	echo urldecode($text); //가나12
 ?>
