3beec91aa9295ebc045080e51e13fa236edf4588
ryungmin <ryungmin@gmail.com>
2015-01-19 15:46:11 +0900

    sscanf()

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-	$var = false;
-	$var ? print('This is TRUE'): print('This is FALSE');
+	$christmas = "2007-12-25";
+
+	$date = sscanf($christmas, "%d-%d-%d");
+	echo "{$date[0]}년 {$date[1]}월 {$date[2]}일은 크리스마스입니다.";
 ?>
