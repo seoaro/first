c31fb9605d3a492315b5bcc27840b7c6f6dac588
ryungmin <ryungmin@gmail.com>
2015-01-19 16:37:06 +0900

    is_array()

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-	$text = '행복한 브라운';
-	echo base64_encode($text) . "<br>";
-	$text = 'x+C6ucfRILrqtvO/7g==';
-	echo base64_decode($text) . "<br>" . "<br>";
-
-	$text = '가나12';
-	echo urlencode($text) . "<br>"; //%B0%A1%B3%AA12
-	$text = '%B0%A1%B3%AA12';
-	echo urldecode($text); //가나12
+	$arr = array('brown','브라운');
+	if(is_array($arr)) echo "주어진 변수는 배열입니다.";//출력
+	else echo "주어진 변수는 배열이 아닙니다.";
 ?>
